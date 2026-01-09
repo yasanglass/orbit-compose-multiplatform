3940d42faac1adfce2fce82b668a3864c808a477
Yasan Glass <yasanglass@gmail.com>
2026-01-09 21:04:28 +0100

    Configure POM metadata for Maven

--- build-logic/conventions/src/main/kotlin/glass/yasan/orbit/buildlogic/PublishPlugin.kt
+++ build-logic/conventions/src/main/kotlin/glass/yasan/orbit/buildlogic/PublishPlugin.kt
@@ -14,6 +14,37 @@ class PublishPlugin : Plugin<Project> {
                 automaticRelease = true,
             )
             signAllPublications()
+
+            pom {
+                name.set("Orbit Compose Multiplatform")
+                description.set("Compose Multiplatform port of Kiwi.com's Orbit design system.")
+                inceptionYear.set("2026")
+                url.set("https://github.com/yasanglass/orbit-compose-multiplatform/")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://github.com/yasanglass/orbit-compose-multiplatform/blob/main/license.md")
+                        distribution.set("https://github.com/yasanglass/orbit-compose-multiplatform/blob/main/license.md")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("yasanglass")
+                        name.set("Yasan Glass")
+                        url.set("https://github.com/yasanglass/")
+                    }
+                    developer {
+                        id.set("contributors")
+                        name.set("All Contributors")
+                        url.set("https://github.com/yasanglass/orbit-compose-multiplatform/graphs/contributors")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/yasanglass/orbit-compose-multiplatform/")
+                    connection.set("scm:git:git://github.com/yasanglass/orbit-compose-multiplatform.git")
+                    developerConnection.set("scm:git:ssh://git@github.com/yasanglass/orbit-compose-multiplatform.git")
+                }
+            }
         }
     }
 }
