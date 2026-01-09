8073c6751d1b459a5ea1a6a0c40d32e1ad21b18b
Yasan Glass <yasanglass@gmail.com>
2026-01-09 19:50:23 +0100

    Downgrade AGP to 8.12.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,6 @@ plugins {
     kotlin("plugin.compose") version "2.3.0" apply false
     id("org.jetbrains.compose") version "1.9.3" apply false
     alias(libs.plugins.compose.hot.reload) apply false
-    id("com.android.library") version "8.13.2" apply false
+    id("com.android.library") version "8.12.0" apply false
     id("com.vanniktech.maven.publish") version "0.35.0" apply false
 }
